b62b62577247ccd3b7171c6344ff890b84de471e
felipe-aguirre <github@aguirre.in>
2022-01-20 14:11:18 -0300

    Proro nuevo

--- broker/broker_mos_eisley.go
+++ broker/broker_mos_eisley.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"math/rand"
 	"net"
+	"strings"
 	"time"
 
 	pb "github.com/felipe-aguirre/tarea_sist_distribuido/protos"
@@ -13,7 +14,8 @@ import (
 
 const (
 	port = ":50050"
-	
+	PLAN = "A"
+	CANTIDAD_SERVIDORES = 2
 )
 var servidores = [...]string{"localhost:50051","localhost:50052","localhost:50053"}
 
@@ -32,35 +34,43 @@ type ManejoComunicacionServer struct {
 // Funcion ReceiveMessage debe tener el mismo nombre en informantes
 func (s *ManejoComunicacionServer) Comunicar(ctx context.Context, in *pb.MessageRequest) (*pb.MessageReply, error) {
 	log.Printf("Se recibió: %v de parte de %v" , in.GetRequest(), in.GetAutor())
-
+	var IP = ""
 	// Plan A
 	// Recordar la IP de cada informante o Leia y redirigirlo al mismo, hasta los 2 minutos
 	// Una conexión en limpio garantiza una asignación aleatoria
-
-	// Desactivar Plan A: Comentar desde Acá
-	// /*
-	var IP = ""
-	if in.GetAutor()!= "Fulcrum" {
+	if in.GetAutor()!= "FulcrumDELETE" {
 		IP = person[in.GetAutor()]
-	if IP == "None" {
-		log.Printf("No hay IP guardada, se elige una aleatoria")
-		// Usar rand int (3) para iterar entre [0,1,2] los 3 servidores fulcrum
-		aleatorio:= random.Intn(2)
-		//Usar aleatorio = 0 mientras para probar con el servidor 1
-	
-		IP = servidores[aleatorio]
-		person[in.GetAutor()] = IP
-		log.Printf("Se le ha asignado la IP %v al usuario %v - Retornando valor de IP", person[in.GetAutor()], in.GetAutor())
-	} else {
-		log.Printf("%v ya tiene la ip %v asignada. - Retornando valor de IP", in.GetAutor(), person[in.GetAutor()])
-	}
-	// */ Hasta acá
+		if PLAN=="A" {
+			if IP == "None" {
+				log.Printf("No hay IP guardada, se elige una aleatoria")
+				aleatorio:= random.Intn(CANTIDAD_SERVIDORES)
+				IP = servidores[aleatorio]
+				person[in.GetAutor()] = IP
+				log.Printf("Se le ha asignado la IP %v al usuario %v - Retornando valor de IP", person[in.GetAutor()], in.GetAutor())
+			} else {
+				log.Printf("%v ya tiene la ip %v asignada. - Retornando valor de IP", in.GetAutor(), person[in.GetAutor()])
+			}
+		} else {
+		// Plan B
+		// Coordinar con los servidores fulcrum quien tiene un reloj de vector igual o más reciente
+		// que el que envía el informante/leia
+		VectorRecibidoInformante := in.GetReloj()
+		respuesta := strings.Split(in.GetRequest(), " ")
+		for i := 0; i < CANTIDAD_SERVIDORES; i++ {
+			conn, err := grpc.Dial(servidores[i], grpc.WithInsecure(), grpc.WithBlock())
+			if err != nil {
+				log.Fatalf("did not connect: %v", err)
+			}
+			c := pb.NewManejoComunicacionClient(conn)
 
-
-	// Plan B
-	// Coordinar con los servidores fulcrum quien tiene un reloj de vector igual o más reciente
-	// que el que envía el informante/leia
-
+			ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+			r, _ := c.ConsultarReloj(ctx, &pb.RelojRequest{Request: respuesta[1]})
+			log.Printf(`Mensaje recibido del Servidor: %s`, r.GetReply())
+			VectorRecibidoServidor := r.GetReply()
+			defer cancel()
+			sum += i
+		}
+		}
 	}
 	
 	if in.GetAutor() == "Fulcrum" {
